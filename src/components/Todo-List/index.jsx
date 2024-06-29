baf641881e536485a52956f8848a06adfef5e2ae
kaMatPrahlad <kamatprahlad43@gmail.com>
2024-06-29 15:42:54 +0530

    added button

--- src/components/Todo-List/index.jsx
+++ src/components/Todo-List/index.jsx
@@ -13,13 +13,16 @@ export default function TodoList() {
     if (!inputvalue) return;
 
     //Check the same matching word in the state
-    if (task.includes(inputvalue)) return;
+    if (task.includes(inputvalue)) {
+      setInputvalue("");
+      return;
+    }
     //Push the value to the new state and store previous value as well
     setTask((prevTask) => [...prevTask, inputvalue]);
     setInputvalue("");
   };
   return (
-    <div className="bg-black h-[80vh] w-full">
+    <div className="bg-black">
       <div className="text-2xl">
         <h1 className="text-4xl mb-5 text-white">Todo List</h1>
         <form action="" onSubmit={handleAdd}>
@@ -30,15 +33,30 @@ export default function TodoList() {
             className="bg-slate-100 rounded-l-lg"
           />
           <button
-            onSubmit
             className="bg-blue-400 border cursor-pointer rounded-r-lg text-white px-4"
+            onSubmit={handleAdd}
           >
             Add
           </button>
         </form>
-
-        <ul>{/* <button className="bg-white">Remove</button> */}</ul>
       </div>
+      <section className="bg-green-300  text-white">
+        <ul className="">
+          {
+            //Map the state and display the value
+            task.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item}
+                  <button className="bg-red-400 border cursor-pointer rounded-lg text-white px-4">
+                    Remove
+                  </button>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </section>
     </div>
   );
 }
