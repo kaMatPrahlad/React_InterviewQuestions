c12d00cd58fe609d860296182f015489c5424096
kaMatPrahlad <kamatprahlad43@gmail.com>
2024-06-30 21:46:21 +0530

    updated state

--- src/components/ParentFolder/Child.jsx
+++ src/components/ParentFolder/Child.jsx
@@ -2,14 +2,22 @@ import React, { useContext } from "react";
 import { UserName } from "../../context/UserName";
 
 export default function Child() {
-  const { myName, myName2 } = useContext(UserName);
+  //   const { myName, myName2 } = useContext(UserName);
+  const { fruit, setFruit } = useContext(UserName);
+  const handleTask = () => {
+    setFruit("apple");
+  };
   return (
     <div className="h-52 border-blue-500 border-2">
       <h1>Im Child Component</h1>
-      <h1 className="text-black">
+      {/* <h1 className="text-black">
         Hi, Im {myName}
         {myName2}
-      </h1>
+      </h1> */}
+      <h2>This Fruit is {fruit}</h2>
+      <button className="px-8 bg-blue-800" onClick={handleTask}>
+        Button{" "}
+      </button>
     </div>
   );
 }
--- src/context/UserName.jsx
+++ src/context/UserName.jsx
@@ -1,19 +1,20 @@
-import { createContext } from "react";
-import { MdFireHydrantAlt } from "react-icons/md";
+import { createContext, useState } from "react";
 
 const UserName = createContext();
 
 const Hero = ({ children }) => {
+  const [fruit, setFruit] = useState("mango");
   //Create the value that you want to show in child component
-  let myName = "Prahlad";
-  let myName2 = "Kamat";
+  //   let myName = "Prahlad";
+  //   let myName2 = "Kamat";
 
   return (
-    <UserName.Provider value={{ myName, myName2 }}>
+    // <UserName.Provider value={{ myName, myName2}}>
+    <UserName.Provider value={{ fruit, setFruit }}>
       {children}
     </UserName.Provider>
   );
 };
 
-//  export default Hero;
+// export { UserName, Hero, fruit, setFruit };
 export { UserName, Hero };
